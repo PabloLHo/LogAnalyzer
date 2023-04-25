39a520d56bad0c1d07050981d9818f2a9b7fe433
PabloLHo <plh00004@red.ujaen.es>
2023-04-25 13:11:54 +0200

    Commit tiempo medio por pagina

--- Ficheros/analisis.py
+++ Ficheros/analisis.py
@@ -31,15 +31,44 @@ def paginasVisitadas(content):
                     else:
                         paginasVisitadas[content[host][usuario]["sesion"][sesion][visita]["pagina"]] = dict()
                         paginasVisitadas[content[host][usuario]["sesion"][sesion][visita]["pagina"]][usuario] = 1
+
                     if usuariosPaginas.keys().__contains__(usuario):
                         if usuariosPaginas[usuario].keys().__contains__(content[host][usuario]["sesion"][sesion][visita]["pagina"]):
-                            usuariosPaginas[usuario][content[host][usuario]["sesion"][sesion][visita]["pagina"]] += 1
+                            usuariosPaginas[usuario][content[host][usuario]["sesion"][sesion][visita]["pagina"]]["visitas"] += 1
+                            inicio = datetime.strptime(content[host][usuario]["sesion"][sesion][visita]["fechaHora"],'%d/%b/%Y:%H:%M:%S')
+                            if (visita + 1) != len(content[host][usuario]["sesion"][sesion]):
+                                fin = datetime.strptime(content[host][usuario]["sesion"][sesion][visita + 1]["fechaHora"], '%d/%b/%Y:%H:%M:%S')
+                            else:
+                                fin = inicio
+                            tiempo = (fin - inicio).total_seconds()
+                            usuariosPaginas[usuario][content[host][usuario]["sesion"][sesion][visita]["pagina"]]["Tiempo"] += tiempo
                         else:
-                            usuariosPaginas[usuario][content[host][usuario]["sesion"][sesion][visita]["pagina"]] = 1
+                            usuariosPaginas[usuario][content[host][usuario]["sesion"][sesion][visita]["pagina"]] = dict()
+                            usuariosPaginas[usuario][content[host][usuario]["sesion"][sesion][visita]["pagina"]]["visitas"] = 1
+                            inicio = datetime.strptime(content[host][usuario]["sesion"][sesion][visita]["fechaHora"],'%d/%b/%Y:%H:%M:%S')
+                            if (visita + 1) != len(content[host][usuario]["sesion"][sesion]):
+                                fin = datetime.strptime(content[host][usuario]["sesion"][sesion][visita + 1]["fechaHora"], '%d/%b/%Y:%H:%M:%S')
+                            else:
+                                fin = inicio
+                            tiempo = (fin - inicio).total_seconds()
+                            usuariosPaginas[usuario][content[host][usuario]["sesion"][sesion][visita]["pagina"]]["Tiempo"] = tiempo
                     else:
                         usuariosPaginas[usuario] = dict()
-                        usuariosPaginas[usuario][content[host][usuario]["sesion"][sesion][visita]["pagina"]] = 1
+                        usuariosPaginas[usuario][content[host][usuario]["sesion"][sesion][visita]["pagina"]] = dict()
+                        usuariosPaginas[usuario][content[host][usuario]["sesion"][sesion][visita]["pagina"]]["visitas"] = 1
+                        inicio = datetime.strptime(content[host][usuario]["sesion"][sesion][visita]["fechaHora"],'%d/%b/%Y:%H:%M:%S')
+                        if (visita + 1) != len(content[host][usuario]["sesion"][sesion]):
+                            fin = datetime.strptime(content[host][usuario]["sesion"][sesion][visita + 1]["fechaHora"],'%d/%b/%Y:%H:%M:%S')
+                        else:
+                            fin = inicio
+                        tiempo = (fin - inicio).total_seconds()
+                        usuariosPaginas[usuario][content[host][usuario]["sesion"][sesion][visita]["pagina"]]["Tiempo"] = tiempo
+
     totalVisitas = dict()
+
+    for usuario in usuariosPaginas:
+        for pagina in usuariosPaginas[usuario]:
+            usuariosPaginas[usuario][pagina]["Media"] = usuariosPaginas[usuario][pagina]["Tiempo"] / usuariosPaginas[usuario][pagina]["visitas"]
 
     for pagina in paginasVisitadas:
         totalVisitas[pagina] = 0
@@ -48,7 +77,7 @@ def paginasVisitadas(content):
 
     res = dict()
     for usuario in usuariosPaginas:
-        diccionario_ordenado = dict(sorted(usuariosPaginas[usuario].items(), key=lambda item: item[1], reverse=True))
+        diccionario_ordenado = dict(sorted(usuariosPaginas[usuario].items(), key=lambda item: item[1]['visitas'], reverse=True))
         res[usuario] = diccionario_ordenado
 
     totalVisitas = dict(sorted(totalVisitas.items(), key=lambda item: item[1], reverse=True))
